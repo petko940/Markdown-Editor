53b0564312214087a7da257fdf85c52f6674c46d
Petko Todorov <p.todorov1015@gmail.com>
2024-07-09 20:01:01 +0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
+import Editor from './components/Editor'
+import Preview from './components/Preview'
+import { TextProvider } from './context/TextContext'
 
 function App() {
 
     return (
-        <>
-            
-        </>
+        <TextProvider>
+            <div className='h-[100vh] bg-slate-400'>
+                <Header />
+
+                <div className='flex justify-between w-2/3 mx-auto'>
+                    <div>
+                        <h1>Editor</h1>
+                        <Editor />
+                    </div>
+                    <div>
+                        <h1>Preview</h1>
+                        <Preview />
+                    </div>
+                </div>
+            </div>
+        </TextProvider>
     )
 }
 
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,7 +1,34 @@
+import { useContext } from "react";
+import { TextContext } from "../context/TextContext";
+import ReactMarkdownEditorLite from "react-markdown-editor-lite";
+import 'react-markdown-editor-lite/lib/index.css';
+import MarkdownIt from 'markdown-it';
+import './Editor.css';
+
 const Editor = () => {
+    const { content, setContent } = useContext(TextContext);
+
+    const mdParser = new MarkdownIt();
+
+    const handleEditorChange = ({ text }) => {
+        setContent(text);
+    };
+
     return (
         <>
-        
+            <ReactMarkdownEditorLite
+                value={content}
+                renderHTML={(text) => mdParser.render(text)}
+                onChange={handleEditorChange}
+                config={{
+                    view: {
+                        menu: true,
+                        md: true,
+                        html: false,
+                    },
+                }}
+                className="h-[60vh]"
+            />
         </>
     )
 };
