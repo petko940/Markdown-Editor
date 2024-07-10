306d36320591c742e94cac4c57ede54c43236266
Petko Todorov <p.todorov1015@gmail.com>
2024-07-10 13:33:17 +0300

    change

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,24 @@
+import { useContext } from 'react';
+import MarkdownPreview from '@uiw/react-markdown-preview';
+import { TextContext } from "../context/TextContext";
+
 const Preview = () => {
+    const { content } = useContext(TextContext);
 
     return (
-        <>
-
-        </>
+        <div className='w-[28vw] h-[60vh] bg-gray-100 p-4 overflow-auto'>
+            {content
+                ? <MarkdownPreview
+                    source={content}
+                    style={{ padding: 20 }}
+                />
+                : <>
+                    <p className='text-center'>
+                        No content
+                    </p>
+                </>}
+        </div>
     )
-};
+}
 
 export default Preview;
--- src/context/TextContext.jsx
+++ src/context/TextContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 const TextContext = createContext();
 
 const TextProvider = ({ children }) => {
-    const [content, setContent] = useState('test1');
+    const [content, setContent] = useState('');
 
     return (
         <TextContext.Provider value={{ content, setContent }}>
