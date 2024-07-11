3d4cb9caf82c710ac2af3d21d185c81e931e9d61
Petko Todorov <p.todorov1015@gmail.com>
2024-07-11 12:00:48 +0300

    added download .md button

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Editor from './components/Editor'
 import Preview from './components/Preview'
 import { TextProvider } from './context/TextContext'
+import Download from './components/Download'
 
 function App() {
 
@@ -11,15 +12,19 @@ function App() {
             <div className='h-[100vh] bg-slate-400'>
                 <Header />
 
-                <div className='flex justify-between gap-5 w-2/3 mx-auto'>
+                <div className='flex justify-center items-center max-lg:justify-center gap-3 w-2/3 max-lg:w-full mx-auto'>
                     <div>
-                        <h1>Editor</h1>
+                        <h1 className='text-center text-teal-50 text-2xl'>Editor</h1>
                         <Editor />
                     </div>
                     <div>
-                        <h1>Preview</h1>
+                        <h1 className='text-center text-teal-50 text-2xl'>Preview</h1>
                         <Preview />
                     </div>
+                </div>
+
+                <div className='flex justify-center'>
+                    <Download />
                 </div>
             </div>
         </TextProvider>
